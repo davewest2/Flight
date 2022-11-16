8b60d8d22adf8058a7edf9da121593369a7579ae
davewest2 <111524826+davewest2@users.noreply.github.com>
2022-11-16 10:13:36 +0000

    Add files via upload
    FlightFetcher and FlighteStatus files as per end of chapter 20

---
+++ FlightFetcher.kt
@@ -0,0 +1,47 @@
+import io.ktor.client.*
+import kotlinx.coroutines.*
+import java.net.URL
+import io.ktor.client.HttpClient
+import io.ktor.client.engine.cio.CIO
+import io.ktor.client.request.get
+
+private const val BASE_URL = "http://kotlin-book.bignerdranch.com/2e"
+private const val FLIGHT_ENDPOINT = "$BASE_URL/flight"
+private const val LOYALTY_ENDPOINT = "$BASE_URL/loyalty"
+
+fun main() {
+    runBlocking {
+        println("STARTED")
+        launch {
+            val flight = fetchFlight("Madrigal")
+            println(flight)
+        }
+    }
+    println("FINISHED")
+}
+
+suspend fun fetchFlight(passengerName: String): FlightStatus = coroutineScope {
+    val client = HttpClient(CIO)
+
+    val flightResponse = async {
+        println("Started fetching flight info")
+        client.get<String>(FLIGHT_ENDPOINT).also {
+            println("Finished fetching flight info")
+            }
+    }
+
+    val loyaltyResponse = async {
+        println("Started fetching loyalty info")
+        client.get<String>(LOYALTY_ENDPOINT).also {
+            println("Finished fetching loyalty info")
+        }
+    }
+
+    delay(500)
+    println("Combining flight data")
+    FlightStatus.parse(
+        passengerName = passengerName,
+        flightResponse = flightResponse.await(),
+        loyaltyResponse = loyaltyResponse.await()
+    )
+}
